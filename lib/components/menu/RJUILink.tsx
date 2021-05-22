088bc1bbeee1422cacb21aee160db50926a9384c
Jayantxu <591060601@qq.com>
2021-05-22 22:38:22 +0800

    modify headLink as Icon in mobileType

--- lib/components/menu/RJUILink.tsx
+++ lib/components/menu/RJUILink.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { mobileSize } from '../../data/mediaJson';
+import { useConfigs } from 'lib/data/context';
+import { Icon } from '../../../components';
 
 const Menu: React.FC<unknown> = () => {
+    const { isMobileType } = useConfigs();
     const redirectGithub = () => {
         window.open(process.env.NEXT_PUBLIC_GITHUB_USER);
     };
     return (
     <div className="rjui-link-header">
         <p className="rjui-font-math front-title">RJUI</p>
-        <div className="rjui-controls rjui-hover" onClick={redirectGithub}>GitHub仓库</div>
+        {isMobileType ? 
+            <div className="rjui-controls rjui-hover" onClick={redirectGithub}>GitHub仓库</div> 
+            : <Icon name="github" style={{ 'marginLeft': '30px' }} onClick={redirectGithub}></Icon>
+        }
         <style global jsx>{`
             .rjui-link-header {
                 display: flex;
@@ -29,6 +35,9 @@ const Menu: React.FC<unknown> = () => {
                 border: 1px solid #d9d9d9;
                 border-radius: 3px;
                 padding: 0.1rem 0.3rem;
+            }
+            .rjui-header-icon {
+                margin-left: 20px;
             }
             @media only screen and (max-width: 1150px) {
                 .rjui-link-header {
